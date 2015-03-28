a18bca71dfc22c06dacaa9bf20f67b997afe9eb9
jselfridge <jselfridge@gmail.com>
2015-03-27 20:26:16 -0400

    Copied repo into 'C-LinAlg-Repo'.

--- TempDir/qr.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ISOMAP_QR
-#define __ISOMAP_QR
-#include "matrix.h"
-
-void gram_schmidt(matrix* a, matrix** q, matrix** r);
-matrix* unitVectorRows(matrix* a);
-matrix* unitVectorColumns(matrix* a);
-
-#endif
