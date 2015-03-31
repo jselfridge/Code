ddefe08d861dd52250426921b7a4acf9ca9d1626
jselfridge <jselfridge@gmail.com>
2015-03-31 17:27:15 -0400

    Removed outdated linear algebra sandbox and repo.

--- LinAlg_Repo/L2_distance.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ISOMAP_L2_DISTANCE
-#define __ISOMAP_L2_DISTANCE
-#include "matrix.h"
-
-matrix* L2_distance(matrix* a, matrix* b);
-
-#endif
--- LinAlg_Repo/svd.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ISOMAP_SVD
-#define __ISOMAP_SVD
-#include "matrix.h"
-
-void svd(matrix* a);
-
-#endif
