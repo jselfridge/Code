c29bb72999a77391672fd7cc229beec454e2c77b
jselfridge <jselfridge@gmail.com>
2015-04-06 11:07:51 -0400

    Linked gnuplot to 'SampleSim'.

--- SampleSim/src/SampleSim.c
+++ SampleSim/src/SampleSim.c
@@ -13,51 +13,47 @@ int main() {
   printf("Starting Sample Simulation!\n");
 
   // Define time variables
-  float duration = 0.1;
+  float duration = 10.0;
   float D = 0.001;
   int N = (int)(duration/D);
 
   // Set up the storage array
-  matrix* simdata = mat_init(2,N+1);
-  matrix* time    = mat_init(1,N+1);
+  matrix* sim  = mat_init(2,N+1);
+  matrix* time = mat_init(1,N+1);
   double  T = 0.0;
   double  E = 0.0;
   matrix* X = mat_init(2,1);
-  mat_set( X,1,1, 1.0 );
-  mat_set( X,2,1, 1.0 );
+  mat_set( X,1,1, 2.0 );
+  mat_set( X,2,1, 0.0 );
 
   // Run the simulation
   for ( int i=1; i<=N; i++ ) {
     for ( int j=1; j<=2; j++ ) {
-      mat_set( simdata,j,i, mat_get(X,j,1) );
+      mat_set( sim,j,i, mat_get(X,j,1) );
     }
     mat_set(time,1,i,T);
     ODE( derivative, &T, &X, D, &E );
     printf("t = %f, x1 = %f, x2 = %f \n", T, mat_get(X,1,1), mat_get(X,2,1) );
   }
   for ( int j=1; j<=2; j++ ) {
-    mat_set( simdata,j,N+1, mat_get(X,j,1) );
+    mat_set( sim,j,N+1, mat_get(X,j,1) );
   }
   mat_set(time,1,N+1,T);
 
-  matrix* plotdata = mat_init(N+1,3);
-  for ( int i=1; i<=N+1; i++ ) {
-    mat_set( plotdata,i,1, mat_get(time,1,i) );
-    for ( int j=1; j<=2; j++ ) {
-      mat_set( plotdata,i,j+1, mat_get(simdata,j,i) );
-    }
-  }
+  // Form plotting matrix
+  matrix* plot = mat_init(N+1,3);
+  plot = mat_appc( mat_trans(time), mat_trans(sim) );
 
-  mat_write(simdata,"data/simdata");
+  // Store the sim data to file
+  mat_write(sim,"data/sim");
   mat_write(time,"data/time");
-  mat_write(plotdata,"data/plotdata");
+  mat_write(plot,"data/plot");
 
+  // Plot the simulation
   system("gnuplot PlotSim");
 
-
-
+  // Complete the simulation
   printf("Program complete\n\n");
-
   return 0 ;
 }
 
@@ -66,8 +62,8 @@ int main() {
 
 matrix* derivative ( double t, matrix* x ) {
   matrix* deriv = mat_init(x->rows,x->cols);
-  double X1 = sin(PI2*t);    mat_set(deriv,1,1,X1);
-  double X2 = -3*exp(-3*t);  mat_set(deriv,2,1,X2);
+  double X1 = -2*exp(-2*t);    mat_set(deriv,1,1,X1);
+  double X2 = (PI2)*cos(PI2*0.2*t);  mat_set(deriv,2,1,X2);
   return deriv;
 }
 
